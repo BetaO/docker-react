002b095ff90d8afb32ab252d12d3741d17f13f14
Oualid Betari <beta.oualid@gmail.com>
2023-05-31 17:04:31 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello there <code>General</code> Kenobi.
         </p>
         <a
           className="App-link"
